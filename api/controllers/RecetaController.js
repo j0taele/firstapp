2efc07795dd3d221c91b6c2810e2b2d7f5405b22
j0taele <jot4ele@gmail.com>
2015-08-28 12:25:04 +0200

    sails generate api receta

---
+++ api/controllers/RecetaController.js
@@ -0,0 +1,11 @@
+/**
+ * RecetaController
+ *
+ * @description :: Server-side logic for managing recetas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
